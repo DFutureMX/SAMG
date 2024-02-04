3002d3ab775f3ced88e27d72a551dc3279d49f23
SeaWar741 <juanca741@gmail.com>
2024-02-04 07:11:20 -0600

    added changes from SEO consultant

--- tailwindconfig.js
+++ tailwindconfig.js
@@ -4,6 +4,7 @@ tailwind.config = {
         colors: {
         darkBlue: '#131b2f',
         yellowAccent: '#fdcd02',
+        yellowAccentHover: '#f8b500',
         borderWidth: {
             '12': '12px',
             // Add more custom border widths as needed
